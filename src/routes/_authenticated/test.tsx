efa1a36a3f2ac5cd17aa8354587e4bac5ed13028
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-06-25 22:27:21 +0000

    feat: enhance authentication test route to display session ID upon successful login

--- src/routes/_authenticated/test.tsx
+++ src/routes/_authenticated/test.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { getSessionDataFn } from "@/utils/authentication";
 
@@ -8,6 +9,7 @@ export const Route = createFileRoute("/_authenticated/test")({
 
 function RouteComponent() {
   const router = useRouter();
+  const [id, setId] = useState("None");
   return (
     <div className="p-4 space-y-4">
       <h2 className="text-xl font-bold">Authentication Middleware Test</h2>
@@ -17,7 +19,10 @@ function RouteComponent() {
       <Button
         onClick={async () => {
           try {
-            await getSessionDataFn();
+            const {
+              session: { id },
+            } = await getSessionDataFn();
+            setId(id);
           } catch (error) {
             if (error) {
               await router.invalidate({ sync: true });
@@ -25,6 +30,11 @@ function RouteComponent() {
           }
         }}
       ></Button>
+
+      <p>
+        <span className="font-semibold">Session ID: </span>
+        {id}
+      </p>
 
       <div className="text-xs text-gray-500 space-y-1">
         <p>• If logged in: Should display session ID</p>
