97b28b56c00d32f027ce0a48c299598666907974
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-06-25 18:58:58 +0000

    fix: restore SITE_URL constant and reorder password length constants in constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
-export const MINIMUM_PASSWORD_LENGTH = 16;
-export const MAXIMUM_PASSWORD_LENGTH = 64;
 export const EVENTUAL_CONSISTENCY_DELAY_S = 60 * 1;
-// export const SITE_URL = "https://tanstack-start.andrei-023.workers.dev";
 // export const IMGGEN_URL = "https://imggen.andrei-023.workers.dev";
+export const MAXIMUM_PASSWORD_LENGTH = 64;
+export const MINIMUM_PASSWORD_LENGTH = 16;
+export const SITE_URL = "https://tanstack-beta.andrei-023.workers.dev/";
 // export const SNAPGEN_PATH = "/api/snapgen";
 export const TEST_USER = {
   name: "Test User",
