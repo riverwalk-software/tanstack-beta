7d11392554cd4d6da79609b3334c00113469620a
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-06-26 15:37:41 +0000

    refactor: simplify theme toggle button labels to single letters

--- src/components/theme/ThemeToggle.tsx
+++ src/components/theme/ThemeToggle.tsx
@@ -8,8 +8,8 @@ export function ThemeToggle() {
     <Button variant="ghost" size="icon" onClick={toggleTheme}>
       {/* <Sun className="!h-[1.2rem] !w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute !h-[1.2rem] !w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" /> */}
-      <span className="material-icons">Light</span>
-      <span className="material-icons dark">Dark</span>
+      <span className="material-icons">L</span>
+      <span className="material-icons dark">D</span>
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
