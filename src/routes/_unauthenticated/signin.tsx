549a01904029f754110e78e1e6a43acc1cb8e554
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-06-26 03:05:48 +0000

    feat: implement SignIn form with validation and improved user feedback

--- src/routes/_unauthenticated/signin.tsx
+++ src/routes/_unauthenticated/signin.tsx
@@ -1,46 +1,177 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useQueryClient } from "@tanstack/react-query";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
-import { useState } from "react";
+import { atom, useAtomValue, useSetAtom } from "jotai";
+import { type UseFormReturn, useForm } from "react-hook-form";
 import { toast } from "sonner";
+import { z } from "zod";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+import { CenteredContainer } from "@/containers/CenteredContainer";
 import { authClient } from "@/lib/auth-client";
 import { authenticationQueryOptions } from "@/utils/authentication";
-import { TEST_USER } from "@/utils/constants";
+import {
+  MAXIMUM_PASSWORD_LENGTH,
+  MINIMUM_PASSWORD_LENGTH,
+} from "@/utils/constants";
 
 export const Route = createFileRoute("/_unauthenticated/signin")({
-  component: RouteComponent,
+  component: SignIn,
 });
 
-function RouteComponent() {
+const isPendingAtom = atom(false);
+
+function SignIn() {
+  return (
+    <CenteredContainer>
+      <MyForm />
+    </CenteredContainer>
+  );
+}
+
+function MyForm() {
   const queryClient = useQueryClient();
   const router = useRouter();
-  const [pending, setPending] = useState(false);
+  const form = useForm<SignInForm>({
+    resolver: zodResolver(SignInFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+  });
+  const setIsPendingAtom = useSetAtom(isPendingAtom);
+  const onSubmit = async (values: SignInForm) => {
+    const formData = SignInFormTransformedSchema.parse(values);
+    const { data, error } = await authClient.signIn.email(formData, {
+      onRequest: () => setIsPendingAtom(true),
+      onSuccess: async () => {
+        toast.success("Sign in successful!");
+        await queryClient.invalidateQueries({
+          queryKey: authenticationQueryOptions.queryKey,
+        });
+        await router.invalidate({ sync: true });
+      },
+      onError: ({ error: { message } }) => {
+        setIsPendingAtom(false);
+        toast.error(message);
+      },
+    });
+    if (error) throw error;
+    return data;
+  };
   return (
-    <Button
-      disabled={pending}
-      onClick={() =>
-        authClient.signIn.email(
-          { ...TEST_USER },
-          {
-            onRequest: () => {
-              setPending(true);
-            },
-            onSuccess: async () => {
-              toast.success("Sign in successful!");
-              await queryClient.invalidateQueries({
-                queryKey: authenticationQueryOptions.queryKey,
-              });
-              await router.invalidate({ sync: true });
-            },
-            onError: (context) => {
-              toast.error(`Sign in failed: ${context.error.message}`);
-              setPending(false);
-            },
-          },
-        )
-      }
-    >
-      Sign In
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormHeader />
+        <FormEmail form={form} />
+        <FormPassword form={form} />
+        <FormRememberMe form={form} />
+        <FormSubmitButton />
+      </form>
+    </Form>
+  );
+}
+function FormHeader() {
+  return (
+    <div className="space-y-2">
+      <h1 className="text-2xl font-bold">Sign In</h1>
+      <FormDescription>
+        Enter your email below to login to your account
+      </FormDescription>
+    </div>
+  );
+}
+
+function FormEmail({ form }: { form: UseFormReturn<SignInForm> }) {
+  return (
+    <FormField
+      control={form.control}
+      name="email"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Email</FormLabel>
+          <FormControl>
+            <Input type="email" placeholder="you@example.com" {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
+
+function FormPassword({ form }: { form: UseFormReturn<SignInForm> }) {
+  return (
+    <FormField
+      control={form.control}
+      name="password"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Password</FormLabel>
+          <FormControl>
+            <Input type="password" placeholder="Password" {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
+
+function FormRememberMe({ form }: { form: UseFormReturn<SignInForm> }) {
+  return (
+    <FormField
+      control={form.control}
+      name="rememberMe"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Remember Me</FormLabel>
+          <FormControl>
+            <Switch checked={field.value} onCheckedChange={field.onChange} />
+          </FormControl>
+        </FormItem>
+      )}
+    />
+  );
+}
+
+function FormSubmitButton() {
+  const isPending = useAtomValue(isPendingAtom);
+  return (
+    <Button disabled={isPending} className="w-full">
+      {isPending ? "Signing in..." : "Sign In"}
     </Button>
   );
 }
+
+const SignInFormSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(
+      MINIMUM_PASSWORD_LENGTH,
+      `Password must be at least ${MINIMUM_PASSWORD_LENGTH} characters long`,
+    )
+    .max(
+      MAXIMUM_PASSWORD_LENGTH,
+      `Password must be at most ${MAXIMUM_PASSWORD_LENGTH} characters long`,
+    ),
+  rememberMe: z.boolean(),
+});
+
+const SignInFormTransformedSchema = SignInFormSchema.transform(
+  (schema) => schema as Parameters<typeof authClient.signIn.email>[0],
+);
+
+type SignInForm = z.infer<typeof SignInFormSchema>;
