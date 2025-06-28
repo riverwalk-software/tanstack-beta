004a71b2edc38718cd6612648f38f667b7ea6276
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-06-28 02:54:02 +0000

    fix: update SITE_URL definition to use SITE_DOMAIN for consistency

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,9 @@ export const EVENTUAL_CONSISTENCY_DELAY_S = 60 * 1;
 // export const IMGGEN_URL = "https://imggen.andrei-023.workers.dev";
 export const MAXIMUM_PASSWORD_LENGTH = 64;
 export const MINIMUM_PASSWORD_LENGTH = 16;
-export const SITE_URL = "https://tanstack-beta.andrei-023.workers.dev/";
+export const SITE_NAME = "TanStack Beta";
+export const SITE_DOMAIN = "tanstack-beta.andrei-023.workers.dev";
+export const SITE_URL = `https://${SITE_DOMAIN}`;
 // export const SNAPGEN_PATH = "/api/snapgen";
 export const TEST_USER = {
   name: "Test User",
