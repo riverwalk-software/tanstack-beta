e37a9b49a11177bf75c476efa23336a0420328c8
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-07-02 22:54:22 +0000

    refactor: remove unused imports and commented-out code in Pokemon route

--- src/routes/_authenticated/pokemon.tsx
+++ src/routes/_authenticated/pokemon.tsx
@@ -1,35 +1,29 @@
-import { useMutation } from "@tanstack/react-query";
 import {
   createFileRoute,
   useRouteContext,
   useRouter,
 } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
 import { useState } from "react";
-import {
-  authenticationQueryOptions,
-  getSessionDataMw,
-} from "@/utils/authentication";
 
 export const Route = createFileRoute("/_authenticated/pokemon")({
   component: Pokemon,
 });
 
-const doStuffFn = createServerFn()
-  .middleware([getSessionDataMw])
-  .handler(async ({ context: { sessionData } }) => {
-    throw new Error("unauthorizeds");
-    return `Hello ${sessionData.user.name ?? "Guest"}!`;
-  });
+// const doStuffFn = createServerFn()
+//   .middleware([getSessionDataMw])
+//   .handler(async ({ context: { sessionData } }) => {
+//     throw new Error("unauthorizeds");
+//     return `Hello ${sessionData.user.name ?? "Guest"}!`;
+//   });
 
 function Pokemon() {
   const { sessionData } = useRouteContext({ from: "/_authenticated" });
   const [pokemonId, setPokemonId] = useState(1);
   const router = useRouter();
-  const doStuff = useMutation({
-    mutationKey: [authenticationQueryOptions.queryKey],
-    mutationFn: () => doStuffFn(),
-  });
+  // const doStuff = useMutation({
+  //   mutationKey: [authenticationQueryOptions.queryKey],
+  //   mutationFn: () => doStuffFn(),
+  // });
   return (
     <div className="grid grid-cols-1 place-items-center bg-blue-200 md:grid-cols-2 lg:grid-cols-3">
       <p className="m-4 bg-red-300 p-6 text-purple-800">Section 1</p>
