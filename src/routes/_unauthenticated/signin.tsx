b14bb6e2572cab19449898f7d3afc3758375911f
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-07-04 00:06:29 +0000

    refactor: rename useOauthFailedEffect to FailedSignInEffect and enhance error handling

--- src/routes/_unauthenticated/signin.tsx
+++ src/routes/_unauthenticated/signin.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
+import ms from "ms";
 import { useEffect } from "react";
 import { toast } from "sonner";
+import { match } from "ts-pattern";
 import { z } from "zod";
 import { SignInWithEmailForm } from "@/components/auth/SignInWithEmailForm";
 import { SignInWithGoogleButton } from "@/components/auth/SignInWithGoogleButton";
@@ -17,7 +19,7 @@ export const Route = createFileRoute("/_unauthenticated/signin")({
 });
 
 function SignIn() {
-  useOauthFailedEffect();
+  FailedSignInEffect();
   return (
     <CenteredContainer>
       <SignInWithEmailForm />
@@ -26,13 +28,26 @@ function SignIn() {
   );
 }
 
-const useOauthFailedEffect = () => {
+const FailedSignInEffect = () => {
   const searchParams = Route.useSearch();
   useEffect(() => {
     const { error } = SearchParamsSchema.parse(searchParams);
-    if (error !== undefined)
-      toast.error("OAuth failed", {
-        description: "Please try again.",
-      });
+    match(error)
+      .with(undefined, () => {})
+      .with("invalid_token", () =>
+        toast.error("Email verification link expired", {
+          // action: {
+          //   label: "Resend verification email",
+          //   onClick: () => resendVerificationEmail(),
+          // },
+          description: "Please try verifying your email again.",
+          duration: ms("30s"),
+        }),
+      )
+      .otherwise(() =>
+        toast.error("Sign in failed", {
+          description: "Please try again.",
+        }),
+      );
   }, [searchParams]);
 };
