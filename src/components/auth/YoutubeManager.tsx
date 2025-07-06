02effec1a43b0808f6726867619f6b71101dac29
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-07-06 22:42:55 +0000

    refactor: improve YoutubeManager component to display authorized channels and handle channel count

--- src/components/auth/YoutubeManager.tsx
+++ src/components/auth/YoutubeManager.tsx
@@ -5,11 +5,33 @@ import { AuthorizeYoutubeButton } from "./AuthorizeYoutubeButton";
 export function YoutubeManager() {
   const {
     youtubeAuthorizationData: { isAuthorized, channelsData },
-  } = useYoutubeAuthorization();
-  return isAuthorized ? <p>Authorized</p> : <AuthorizeYoutubeButton />;
+  } = useYoutubeAuthorizationData();
+  return !isAuthorized ? (
+    <AuthorizeYoutubeButton />
+  ) : (
+    <div>
+      <h3>Authorized Channels</h3>
+      <ul>
+        {channelsData &&
+          (channelsData.channelCount === "zero" ? (
+            <li>No channels authorized</li>
+          ) : channelsData.channelCount === "one" ? (
+            <li>
+              {channelsData.channels.snippet.title} ({channelsData.channels.id})
+            </li>
+          ) : (
+            channelsData.channels.map((channel) => (
+              <li key={channel.id}>
+                {channel.snippet.title} ({channel.id})
+              </li>
+            ))
+          ))}
+      </ul>
+    </div>
+  );
 }
 
-const useYoutubeAuthorization = () => {
+const useYoutubeAuthorizationData = () => {
   const { data: youtubeAuthorizationData } = useSuspenseQuery(
     youtubeAuthorizationDataQueryOptions,
   );
