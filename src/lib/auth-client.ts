c1461f3b7414962e736414a9e7e75999f89f55d9
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-07-22 23:14:31 +0000

    refactor: enhance sign-out process by updating signOut logic and adding cookie removal functionality

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,18 @@
+import type { QueryClient } from "@tanstack/query-core";
+import type { useRouter } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import { deleteCookie } from "@tanstack/react-start/server";
 import { type BetterFetchError, createAuthClient } from "better-auth/react";
+import { AUTH_COOKIE_NAMES, AUTH_COOKIE_PREFIX } from "@/utils/constants";
 
-const { signOut: oldSignOut, ...rest } = createAuthClient({
+const {
+  $ERROR_CODES,
+  linkSocial,
+  sendVerificationEmail,
+  signIn,
+  signOut: _signOut,
+  signUp,
+} = createAuthClient({
   fetchOptions: {
     retry: 0,
     throw: true,
@@ -10,18 +22,38 @@ const { signOut: oldSignOut, ...rest } = createAuthClient({
   },
 });
 
+const removeAuthCookiesFn = createServerFn({ method: "POST" }).handler(
+  async () => {
+    AUTH_COOKIE_NAMES.forEach((cookieName) =>
+      deleteCookie(`${AUTH_COOKIE_PREFIX}.${cookieName}`),
+    );
+  },
+);
+
 export const authClient = {
-  ...rest,
+  linkSocial,
+  sendVerificationEmail,
+  signIn,
+  signUp,
   signOut: async () => {
     try {
-      await oldSignOut();
+      await _signOut();
     } catch (error) {
       console.error("Error during sign out:", error);
+      await removeAuthCookiesFn(); // Does not throw
     }
   },
 };
 
-type BetterAuthErrorCode = keyof typeof authClient.$ERROR_CODES;
+export const afterSignOut = async (
+  queryClient: QueryClient,
+  router: ReturnType<typeof useRouter>,
+): Promise<void> => {
+  queryClient.clear();
+  await router.invalidate({ sync: true });
+};
+
+type BetterAuthErrorCode = keyof typeof $ERROR_CODES;
 type BetterAuthErrorContext = {
   error: (BetterFetchError & Record<string, any>) & {
     code: BetterAuthErrorCode;
