4518f49c3f215fb33963119b1d8f78f1c36b9755
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-07-25 04:35:50 +0000

    feat: add user tables and update snapshot and journal metadata

---
+++ drizzle/0001_chief_edwin_jarvis.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users_table2` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`age` integer NOT NULL,
+	`email` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_table2_email_unique` ON `users_table2` (`email`);
---
+++ drizzle/0002_parallel_the_hunter.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users_table3` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`age` integer NOT NULL,
+	`email` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_table3_email_unique` ON `users_table3` (`email`);
