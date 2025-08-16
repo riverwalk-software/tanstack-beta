4d700853be49921fa6f4b56865de4fa07dfe3f04
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-08-16 00:55:47 +0000

    refactor: update package scripts and dependencies; enhance Vite configuration with MDX and expressive code plugins
    - Changed script names for type generation to be more concise.
    - Updated several dependencies to their latest versions, including @tanstack packages and added new dependencies like @mdx-js/react and @polar-sh/sdk.
    - Modified globals.css to include shadcn-prose for improved styling.
    - Enhanced vite.config.ts to integrate MDX support and expressive code plugins, including collapsible sections and line numbers, with custom rehype options.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,61 @@
+import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+import mdx from "@mdx-js/rollup";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { pluginFullscreen } from "expressive-code-fullscreen";
+import rehypeExpressiveCode, {
+  type RehypeExpressiveCodeOptions,
+} from "rehype-expressive-code";
+import rehypeKatex from "rehype-katex";
+import rehypeMermaid from "rehype-mermaid";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+
+const rehypeExpressiveCodeOptions: RehypeExpressiveCodeOptions = {
+  themes: ["github-light-default", "github-dark-default"],
+  customizeTheme: (theme) => {
+    theme.name = theme.name === "github-light-default" ? "light" : "dark";
+    return theme;
+  },
+  useDarkModeMediaQuery: false,
+  defaultProps: {
+    wrap: false,
+    overridesByLang: {
+      shell: {
+        preserveIndent: false,
+        showLineNumbers: false,
+      },
+    },
+  },
+  frames: {
+    extractFileNameFromCode: false,
+  },
+  plugins: [
+    pluginLineNumbers(),
+    pluginCollapsibleSections(),
+    pluginFullscreen(),
+  ],
+};
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
+    {
+      enforce: "pre",
+      ...mdx({
+        remarkPlugins: [remarkGfm, remarkMath],
+        rehypePlugins: [
+          rehypeMermaid, // Must come before rehypeExpressiveCode
+          rehypeKatex, // Must come before rehypeExpressiveCode
+          [rehypeExpressiveCode, rehypeExpressiveCodeOptions],
+        ],
+      }),
+    },
     tsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
@@ -15,6 +63,6 @@ export default defineConfig({
       target: "cloudflare-module",
       customViteReactPlugin: true,
     }),
-    viteReact(),
+    viteReact({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
   ],
 });
