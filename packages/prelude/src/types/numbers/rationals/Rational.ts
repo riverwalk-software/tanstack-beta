65755cbf0413bb13a1cea5855703ecb9f9555726
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-08-28 18:34:41 +0000

    Refactor Rational type to use Schema for validation and update implementation

--- packages/prelude/src/types/numbers/rationals/Rational.ts
+++ packages/prelude/src/types/numbers/rationals/Rational.ts
@@ -1,11 +1,8 @@
-import z from "zod"
+import { Schema } from "effect"
 import { IntegerSchema } from "../integers/Integer"
 import { NonZeroIntegerSchema } from "../integers/NonZeroInteger"
 
-export const RationalSchema = z
-  .object({
-    numerator: IntegerSchema,
-    denominator: NonZeroIntegerSchema,
-  })
-  .brand("Rational")
-export type Rational = z.infer<typeof RationalSchema>
+export class Rational extends Schema.Class<Rational>("Rational")({
+  numerator: IntegerSchema,
+  denominator: NonZeroIntegerSchema,
+}) {}
