af72cf267b6e22d5ea7e947610c259237c999854
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-08-28 18:32:53 +0000

    Refactor Natural type to use Brand for validation and update schema definition

--- packages/prelude/src/types/numbers/naturals/Natural.ts
+++ packages/prelude/src/types/numbers/naturals/Natural.ts
@@ -1,5 +1,12 @@
-import type z from "zod"
-import { IntegerSchema } from "../integers/Integer"
+import Effect, { Brand, Schema } from "effect"
+import { Integer } from "../integers/Integer"
+import type { Real } from "../reals/Real"
 
-export const NaturalSchema = IntegerSchema.nonnegative().brand("Natural")
-export type Natural = z.infer<typeof NaturalSchema>
+type _Natural = Real & Brand.Brand<"Natural">
+const _Natural = Brand.refined<_Natural>(
+  Effect.Number.greaterThanOrEqualTo(0),
+  n => Brand.error(`Expected ${n} to be a non-negative Integer`),
+)
+export const Natural = Brand.all(_Natural, Integer)
+export type Natural = Brand.Brand.FromConstructor<typeof Natural>
+export const NaturalSchema = Schema.Number.pipe(Schema.fromBrand(Natural))
