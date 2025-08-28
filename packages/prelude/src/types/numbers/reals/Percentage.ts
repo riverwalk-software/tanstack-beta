1267cb460d9b36cc0d6b356c0a3c453eba651b78
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-08-28 18:11:37 +0000

    Refactor Percentage type to use Brand for validation and update schema definition

--- packages/prelude/src/types/numbers/reals/Percentage.ts
+++ packages/prelude/src/types/numbers/reals/Percentage.ts
@@ -1,5 +1,6 @@
-import type z from "zod"
-import { RealSchema } from "./Real"
+import { Brand, Schema } from "effect"
+import type { Real } from "./Real"
 
-export const PercentageSchema = RealSchema.brand("Percentage")
-export type Percentage = z.infer<typeof PercentageSchema>
+export type Percentage = Real & Brand.Brand<"Percentage">
+export const Percentage = Brand.nominal<Percentage>()
+export const PercentageSchema = Schema.Number.pipe(Schema.fromBrand(Percentage))
