3a4c84d687bc78766d5d76e99c33790c9c482b36
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-08-28 18:12:23 +0000

    Refactor BoundedPercentage type to use Brand for validation and update schema definition

--- packages/prelude/src/types/numbers/reals/BoundedPercentage.ts
+++ packages/prelude/src/types/numbers/reals/BoundedPercentage.ts
@@ -1,26 +1,31 @@
-import type z from "zod"
-import { pipe } from "../../../logic/combinators"
-import { size } from "../../../typeclasses/functors/Foldable"
-import { count } from "../../../typeclasses/functors/MonadAlternative"
-import { divide } from "../../../typeclasses/rings/Field"
-import type { NonEmptyList } from "../../lists/nonEmptyList"
-import { NonZeroRealSchema } from "./NonZeroReal"
-import { PercentageSchema } from "./Percentage"
-import {
-  UnitIntervalSchema,
-  unitIntervalBoundedPercentageBijection,
-} from "./UnitInterval"
+import Effect, { Brand, Schema } from "effect"
+import { Percentage } from "./Percentage"
+import type { Real } from "./Real"
 
-export const BoundedPercentageSchema = PercentageSchema.min(0)
-  .max(100)
-  .brand("BoundedPercentage")
-export type BoundedPercentage = z.infer<typeof BoundedPercentageSchema>
+type _BoundedPercentage = Real & Brand.Brand<"BoundedPercentage">
+const _BoundedPercentage = Brand.refined<_BoundedPercentage>(
+  Effect.Number.between({
+    minimum: 0,
+    maximum: 100,
+  }),
+  n =>
+    Brand.error(
+      `Expected ${n} to be a BoundedPercentage in the interval [0, 100]`,
+    ),
+)
+export const BoundedPercentage = Brand.all(_BoundedPercentage, Percentage)
+export type BoundedPercentage = Brand.Brand.FromConstructor<
+  typeof BoundedPercentage
+>
+export const BoundedPercentageSchema = Schema.Number.pipe(
+  Schema.fromBrand(BoundedPercentage),
+)
 
-export const proportionOf =
-  <A>(p: (a: A) => boolean) =>
-  (xs: NonEmptyList<A>): BoundedPercentage => {
-    const part = count(p)(xs)
-    const whole = pipe(xs, size, NonZeroRealSchema.parse)
-    const quotient = pipe(divide(part)(whole), UnitIntervalSchema.parse)
-    return unitIntervalBoundedPercentageBijection.to(quotient)
-  }
+// export const proportionOf =
+//   <A>(p: (a: A) => boolean) =>
+//   (xs: NonEmptyList<A>): BoundedPercentage => {
+//     const part = count(p)(xs)
+//     const whole = pipe(xs, size, Schema.decodeSync(NonZeroReal))
+//     const quotient = pipe(divide(part)(whole), Schema.decodeSync(UnitInterval))
+//     return unitIntervalBoundedPercentageBijection.to(quotient)
+//   }
