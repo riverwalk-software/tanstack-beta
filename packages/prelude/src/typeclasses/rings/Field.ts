9033c7d6c470d480eef5ed96e9ab97768778d875
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-08-28 18:38:57 +0000

    Rename divide function to realDivide for clarity and maintain consistency in naming

--- packages/prelude/src/typeclasses/rings/Field.ts
+++ packages/prelude/src/typeclasses/rings/Field.ts
@@ -1,7 +1,16 @@
+import type { Integer } from "../../types/numbers/integers/Integer"
+import type { NonZeroInteger } from "../../types/numbers/integers/NonZeroInteger"
+import { Rational } from "../../types/numbers/rationals/Rational"
 import type { NonZeroReal } from "../../types/numbers/reals/NonZeroReal"
 import type { Real } from "../../types/numbers/reals/Real"
 
-export const divide =
+export const realDivide =
   (dividend: Real) =>
   (divisor: NonZeroReal): Real =>
     dividend / divisor
+
+export const divide = (dividend: Integer, divisor: NonZeroInteger): Rational =>
+  new Rational({
+    numerator: dividend,
+    denominator: divisor,
+  })
