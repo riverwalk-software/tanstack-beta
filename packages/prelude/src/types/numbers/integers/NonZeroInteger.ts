7d093160232d48f92016f2aa08a037ca326283de
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-08-28 18:28:43 +0000

    Refactor NonZeroInteger type to use Brand for validation and update schema definition

--- packages/prelude/src/types/numbers/integers/NonZeroInteger.ts
+++ packages/prelude/src/types/numbers/integers/NonZeroInteger.ts
@@ -1,7 +1,16 @@
-import type z from "zod"
-import { IntegerSchema } from "./Integer"
+import { Brand, flow, Schema } from "effect"
+import { not } from "../../../typeclasses/lattices/BooleanLattice"
+import { areEqual } from "../../../typeclasses/lattices/Eq"
+import type { Real } from "../reals/Real"
+import { Integer } from "./Integer"
 
-export const NonZeroIntegerSchema = IntegerSchema.refine(n => n !== 0, {
-  message: "Number must be a non-zero integer",
-}).brand("NonZeroInteger")
-export type NonZeroInteger = z.infer<typeof NonZeroIntegerSchema>
+type _NonZeroInteger = Real & Brand.Brand<"NonZeroInteger">
+const _NonZeroInteger = Brand.refined<_NonZeroInteger>(
+  flow(areEqual(0), not),
+  n => Brand.error(`Expected ${n} to be a non-zero Integer`),
+)
+export const NonZeroInteger = Brand.all(_NonZeroInteger, Integer)
+export type NonZeroInteger = Brand.Brand.FromConstructor<typeof NonZeroInteger>
+export const NonZeroIntegerSchema = Schema.Number.pipe(
+  Schema.fromBrand(NonZeroInteger),
+)
