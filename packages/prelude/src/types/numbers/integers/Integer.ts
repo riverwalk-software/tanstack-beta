50a217946da63230e9d52c06a55aa2b45de30413
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-08-28 18:23:21 +0000

    Refactor Integer type to use Brand for validation and update schema definition

--- packages/prelude/src/types/numbers/integers/Integer.ts
+++ packages/prelude/src/types/numbers/integers/Integer.ts
@@ -1,5 +1,8 @@
-import type z from "zod"
-import { RealSchema } from "../reals/Real"
+import { Brand, Schema } from "effect"
+import type { Real } from "../reals/Real"
 
-export const IntegerSchema = RealSchema.int().brand("Integer")
-export type Integer = z.infer<typeof IntegerSchema>
+export type Integer = Real & Brand.Brand<"Integer">
+export const Integer = Brand.refined<Integer>(Number.isSafeInteger, n =>
+  Brand.error(`Expected ${n} to be a Real restricted to safe integers`),
+)
+export const IntegerSchema = Schema.Number.pipe(Schema.fromBrand(Integer))
