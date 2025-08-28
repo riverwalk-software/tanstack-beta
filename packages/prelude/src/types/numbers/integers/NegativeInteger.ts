63ec56ff7e26bae0543bd13ae4075ce392daff80
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-08-28 18:30:44 +0000

    Refactor NegativeInteger and PositiveInteger types to use Brand for validation and update schema definitions

--- packages/prelude/src/types/numbers/integers/NegativeInteger.ts
+++ packages/prelude/src/types/numbers/integers/NegativeInteger.ts
@@ -1,6 +1,16 @@
-import type z from "zod"
-import { IntegerSchema } from "./Integer"
+import Effect, { Brand, Schema } from "effect"
+import type { Real } from "../reals/Real"
+import { Integer } from "./Integer"
 
-export const NegativeIntegerSchema =
-  IntegerSchema.negative().brand("NegativeInteger")
-export type NegativeInteger = z.infer<typeof NegativeIntegerSchema>
+type _NegativeInteger = Real & Brand.Brand<"NegativeInteger">
+const _NegativeInteger = Brand.refined<_NegativeInteger>(
+  Effect.Number.lessThan(0),
+  n => Brand.error(`Expected ${n} to be a negative Integer`),
+)
+export const NegativeInteger = Brand.all(_NegativeInteger, Integer)
+export type NegativeInteger = Brand.Brand.FromConstructor<
+  typeof NegativeInteger
+>
+export const NegativeIntegerSchema = Schema.Number.pipe(
+  Schema.fromBrand(NegativeInteger),
+)
--- packages/prelude/src/types/numbers/integers/PositiveInteger.ts
+++ packages/prelude/src/types/numbers/integers/PositiveInteger.ts
@@ -1,6 +1,16 @@
-import type z from "zod"
-import { IntegerSchema } from "./Integer"
+import Effect, { Brand, Schema } from "effect"
+import type { Real } from "../reals/Real"
+import { Integer } from "./Integer"
 
-export const PositiveIntegerSchema =
-  IntegerSchema.positive().brand("PositiveInteger")
-export type PositiveInteger = z.infer<typeof PositiveIntegerSchema>
+type _PositiveInteger = Real & Brand.Brand<"PositiveInteger">
+const _PositiveInteger = Brand.refined<_PositiveInteger>(
+  Effect.Number.greaterThan(0),
+  n => Brand.error(`Expected ${n} to be a positive Integer`),
+)
+export const PositiveInteger = Brand.all(_PositiveInteger, Integer)
+export type PositiveInteger = Brand.Brand.FromConstructor<
+  typeof PositiveInteger
+>
+export const PositiveIntegerSchema = Schema.Number.pipe(
+  Schema.fromBrand(PositiveInteger),
+)
