ba2687104890db305531078dda3290e34fe06321
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-08-28 18:03:18 +0000

    Refactor NegativeReal type to use Brand and Effect for validation

--- packages/prelude/src/types/numbers/reals/NegativeReal.ts
+++ packages/prelude/src/types/numbers/reals/NegativeReal.ts
@@ -1,5 +1,11 @@
-import type z from "zod"
-import { RealSchema } from "./Real"
+import Effect, { Brand } from "effect"
+import { NonZeroReal } from "./NonZeroReal"
+import type { Real } from "./Real"
 
-export const NegativeRealSchema = RealSchema.negative().brand("NegativeReal")
-export type NegativeReal = z.infer<typeof NegativeRealSchema>
+type _NegativeReal = Real & Brand.Brand<"NegativeReal">
+const _NegativeReal = Brand.refined<_NegativeReal>(
+  Effect.Number.lessThan(0),
+  n => Brand.error(`Expected ${n} to be a negative real number`),
+)
+export const NegativeReal = Brand.all(_NegativeReal, NonZeroReal)
+export type NegativeReal = Brand.Brand.FromConstructor<typeof NegativeReal>
